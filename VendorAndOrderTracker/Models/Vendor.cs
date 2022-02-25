33b52b00e58464b535f9e91a2f30d8f239d4a0aa
Christen Weston <paleeserecycle@gmail.com>
2022-02-25 12:37:11 -0800

    Initial Vendor class created

---
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{
+  public class Vendor
+  {
+    public string Name {get; set; }
+    public string Description {get; set; }
+    public static List<Vendor> _instances = new List<Vendor> {};
+    public int Id {get; }
+
+
+    public Vendor(string vendorName, string description)
+    {
+      // Name = vendorName;
+      // Description = description;
+      // _instances.Add(this);
+      // Id = _instances.Count;
+    }
+
+
+  }
+}
