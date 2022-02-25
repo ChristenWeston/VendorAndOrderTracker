5096e146c1923b061a9a1c26ad6d8580b18b1120
Christen Weston <paleeserecycle@gmail.com>
2022-02-25 15:51:10 -0800

    Added in New view and Controller to this New view

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -14,5 +14,19 @@ namespace VendorAndOrderTracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
   }
 }
