a9cddf2898a99ed4acee4c37893d34f9cd3388d5
Christen Weston <paleeserecycle@gmail.com>
2022-02-25 15:11:09 -0800

    Added in Home Controller

---
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendorsAndOrders.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
