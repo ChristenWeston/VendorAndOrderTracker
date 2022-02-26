ebdae372563c016d494b9c9f12885ad4ecf20c61
Christen Weston <paleeserecycle@gmail.com>
2022-02-25 16:46:45 -0800

    10th test for an empty order list passing

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -44,5 +44,15 @@ namespace VendorAndOrderTracker.Tests
 
       Assert.AreEqual(updatedDescription, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
